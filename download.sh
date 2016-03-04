c044ccd20ce0d44810748e9ed30eaae9262ba9d5
Minjoon Seo <seominjoon@gmail.com>
2016-03-03 23:00:43 -0800

    bug fix download

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-DQA_DATA_DIR="~/data/dqa"
-wget https://s3-us-west-2.amazonaws.com/dqa-data/shining3-1500r.zip $DQA_DATA_DIR
-unzip $DQA_DATA_DIR/shining3-1500r $DQA_DATA_DIR
+DQA_DATA_DIR="$HOME/data/dqa"
+wget https://s3-us-west-2.amazonaws.com/dqa-data/shining3-1500r.zip -O $DQA_DATA_DIR/shining3-1500r.zip
+unzip $DQA_DATA_DIR/shining3-1500r.zip -d $DQA_DATA_DIR
