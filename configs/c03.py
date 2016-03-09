f1cc1307b3227649a6de02a27539205a948d216d
Minjoon Seo <seominjoon@gmail.com>
2016-03-08 22:54:45 -0800

    small config change

--- configs/c03.py
+++ configs/c03.py
@@ -13,6 +13,24 @@ configs[1] = {'batch_size': 100,
               'max_grad_norm': 40,
               'keep_prob': 0.5,
               'mode': 'l',
+              'data_dir': 'data/s3_03',
+              'fold_path': "data/s3/folds/fold11.json"}
+
+configs[2] = {'batch_size': 30,
+              'hidden_size': 100,
+              'num_layers': 1,
+              'rnn_num_layers': 2,
+              'init_mean': 0,
+              'init_std': 0.1,
+              'init_lr': 0.01,
+              'anneal_period': 100,
+              'anneal_ratio': 0.5,
+              'num_epochs': 50,
+              'linear_start': False,
+              'max_grad_norm': 40,
+              'keep_prob': 0.5,
+              'mode': 'lca',
+              'data_dir': 'data/s3_03',
               'fold_path': "data/s3/folds/fold11.json"}
 
 
