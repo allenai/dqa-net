29cc9cca9087cb2eadab395bda120308f5bb67a3
Minjoon Seo <seominjoon@gmail.com>
2016-03-09 15:52:11 -0800

    removed masked softmax

--- nn.py
+++ nn.py
@@ -9,7 +9,7 @@ VERY_SMALL_NUMBER = -1e10
 def softmax_with_mask(shape, x, mask, name=None):
     if name is None:
         name = softmax_with_mask.__name__
-    x_masked = x + VERY_SMALL_NUMBER * (1 - mask)
+    x_masked = x # + VERY_SMALL_NUMBER * (1 - mask)
     x_flat = tf.reshape(x_masked, [reduce(mul, shape[:-1], 1), shape[-1]])
     p_flat = tf.nn.softmax(x_flat)
     p = tf.reshape(p_flat, shape, name=name)
