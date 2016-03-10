038debe71dc42acacce89e369a3331a4e25efdc4
Minjoon Seo <seominjoon@gmail.com>
2016-03-10 02:48:14 -0800

    relation vis done

---
+++ vis/list_facts.py
@@ -0,0 +1,127 @@
+import shutil
+
+import SimpleHTTPServer
+import SocketServer
+import argparse
+import json
+import os
+from copy import deepcopy
+
+from jinja2 import Environment, FileSystemLoader
+
+from utils import get_pbar
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("prepro_dir")
+    parser.add_argument("--start", default=0, type=int)
+    parser.add_argument("--stop", default=1500, type=int)
+    parser.add_argument("--show_im", default='True')
+    parser.add_argument("--im_width", type=int, default=200)
+    parser.add_argument("--ext", type=str, default=".png")
+    parser.add_argument("--template_name", type=str, default="list_facts.html")
+    parser.add_argument("--num_im", type=int, default=50)
+    parser.add_argument("--port", type=int, default=8000)
+    parser.add_argument("--host", type=str, default="0.0.0.0")
+    parser.add_argument("--open", type=str, default='False')
+
+    args = parser.parse_args()
+    return args
+
+
+def _decode_sent(decoder, sent):
+    return " ".join(decoder[idx] for idx in sent)
+
+
+
+def list_facts(args):
+    prepro_dir = args.prepro_dir
+    meta_data_dir = os.path.join(prepro_dir, "meta_data.json")
+    meta_data = json.load(open(meta_data_dir, "r"))
+    data_dir = meta_data['data_dir']
+    _id = 0
+    html_dir = "/tmp/list_facts%d" % _id
+    while os.path.exists(html_dir):
+        _id += 1
+        html_dir = "/tmp/list_facts%d" % _id
+
+    images_dir = os.path.join(data_dir, 'images')
+    annos_dir = os.path.join(data_dir, 'annotations')
+
+    sents_path = os.path.join(prepro_dir, 'sents.json')
+    facts_path = os.path.join(prepro_dir, 'facts.json')
+    vocab_path = os.path.join(prepro_dir, 'vocab.json')
+    answers_path = os.path.join(prepro_dir, 'answers.json')
+    sentss_dict = json.load(open(sents_path, "r"))
+    facts_dict = json.load(open(facts_path, "r"))
+    vocab = json.load(open(vocab_path, "r"))
+    answers_dict = json.load(open(answers_path, "r"))
+    decoder = {idx: word for word, idx in vocab.items()}
+
+    if os.path.exists(html_dir):
+        shutil.rmtree(html_dir)
+    os.mkdir(html_dir)
+
+    cur_dir = os.path.dirname(os.path.realpath(__file__))
+    templates_dir = os.path.join(cur_dir, 'templates')
+    env = Environment(loader=FileSystemLoader(templates_dir))
+    template = env.get_template(args.template_name)
+
+    headers = ['iid', 'qid', 'image', 'sents', 'answer', 'annotations', 'relations']
+    rows = []
+    pbar = get_pbar(len(sentss_dict)).start()
+    image_ids = sorted(sentss_dict.keys(), key=lambda x: int(x))
+    for i, image_id in enumerate(image_ids):
+        sentss = sentss_dict[image_id]
+        answers = answers_dict[image_id]
+        facts = facts_dict[image_id]
+        decoded_facts = [_decode_sent(decoder, fact) for fact in facts]
+        for question_id, (sents, answer) in enumerate(zip(sentss, answers)):
+            image_name = "%s.png" % image_id
+            json_name = "%s.json" % image_name
+            image_url = os.path.join('images', image_name)
+            anno_url = os.path.join('annotations', json_name)
+            row = {'image_id': image_id,
+                   'question_id': question_id,
+                   'image_url': image_url,
+                   'anno_url': anno_url,
+                   'sents': [_decode_sent(decoder, sent) for sent in sents],
+                   'answer': answer,
+                   'facts': decoded_facts}
+            rows.append(row)
+
+        if i % args.num_im == 0:
+            html_path = os.path.join(html_dir, "%s.html" % str(image_id).zfill(8))
+
+        if (i + 1) % args.num_im == 0 or (i + 1) == len(image_ids):
+            var_dict = {'title': "Question List",
+                        'image_width': args.im_width,
+                        'headers': headers,
+                        'rows': rows,
+                        'show_im': True if args.show_im == 'True' else False}
+            with open(html_path, "wb") as f:
+                f.write(template.render(**var_dict).encode('UTF-8'))
+            rows = []
+        pbar.update(i)
+    pbar.finish()
+
+    os.system("ln -s %s/* %s" % (data_dir, html_dir))
+    os.chdir(html_dir)
+    port = args.port
+    host = args.host
+    # Overriding to suppress log message
+    class MyHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+        def log_message(self, format, *args):
+            pass
+    handler = MyHandler
+    httpd = SocketServer.TCPServer((host, port), handler)
+    if args.open == 'True':
+        os.system("open http://%s:%d" % (args.host, args.port))
+    print("serving at %s:%d" % (host, port))
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    ARGS = get_args()
+    list_facts(ARGS)
